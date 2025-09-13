42203917dbd085a47af38b839911c07cb139b28e
saibaldas <saibal_das@hotmail.com>
2025-09-13 15:05:18 -0500

    Send Correlation ID

--- app/main.go
+++ app/main.go
@@ -1,29 +1,53 @@
 package main
 
 import (
+	"bytes"
 	"fmt"
+	"io"
 	"net"
 	"os"
 )
 
-// Ensures gofmt doesn't remove the "net" and "os" imports in stage 1 (feel free to remove this!)
-var _ = net.Listen
-var _ = os.Exit
-
 func main() {
-	// You can use print statements as follows for debugging, they'll be visible when running tests.
 	fmt.Println("Logs from your program will appear here!")
-
-	//Uncomment this block to pass the first stage
 
 	l, err := net.Listen("tcp", "0.0.0.0:9092")
 	if err != nil {
 		fmt.Println("Failed to bind to port 9092")
 		os.Exit(1)
 	}
-	_, err = l.Accept()
-	if err != nil {
-		fmt.Println("Error accepting connection: ", err.Error())
-		os.Exit(1)
+	defer l.Close()
+
+	fmt.Println("Start listening on 9092")
+	for {
+		conn, err := l.Accept()
+		if err != nil {
+			fmt.Println("Error accepting connection: ", err.Error())
+			os.Exit(1)
+		}
+		fmt.Println("Connection accepted on 9092")
+		go handleConnection(conn)
 	}
+}
+
+func handleConnection(conn net.Conn) {
+	defer conn.Close()
+
+	received := bytes.Buffer{}
+	buff := make([]byte, 1024)
+
+	fmt.Println("Connection read on 9092")
+
+	_, err := conn.Read(buff)
+	if err != nil && err == io.EOF {
+		fmt.Printf("failed to read from connection: %v", err)
+	}
+
+	fmt.Println("Connection read done on 9092")
+
+	received.Write(buff)
+	// request := request_decoder.DecodeRequest(received.Bytes())
+	// response := handler.HandlerRequest(request)
+
+	conn.Write([]byte{0, 0, 0, 0, 0, 0, 0, 7})
 }
